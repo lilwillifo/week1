e433036b1148824cabd993a02aa1f249ba7a19e9
lilwillifo <margaretwilliford4@gmail.com>
2018-01-23 22:24:30 -0700

    added escape strings

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def first_last(first, *middle, last)
   puts "This first argument is #{first}"
   puts "The last argument is #{last}"
@@ -6,6 +8,7 @@ end
 first_last(1,2,3,4)
 
 animals = ["dog", "cat", "goat", "capybara"]
+#or shortcut: animals = %w(dog cat goat capybara)
 animals_capitalized = animals.map {|animal| animal.capitalize}
 
 puts animals_capitalized
@@ -17,3 +20,8 @@ def single_string(array)
 end
 
 puts single_string(animals_capitalized)
+
+#escape strings
+escape = "Margaret \n5'5\""
+puts escape
+escape.split #returns ["Margaret", "5'5\""]
